ab8997a4728560cdaf17af8a1f625715061d2ad9
Jacob Truman <jacob.truman@gmail.com>
2018-06-26 15:22:58 -0600

    Fix Saliens game

--- web.py
+++ web.py
@@ -9,8 +9,6 @@ from bs4 import BeautifulSoup
 from steam.guard import SteamAuthenticator
 import steam.webauth as wa
 
-update_check = 7
-
 
 def get_all_config_files(configs_dir):
     if configs_dir is not None and os.path.exists(configs_dir):
@@ -113,23 +111,23 @@ def get_zone(session):
                     return str(zone["zone_position"]), planet["id"], planet["state"]["name"], difficulty
 
 
-def get_user_info(session):
+def get_user_info(session, update_check):
     data = {'access_token': get_game_token(session)}
     result = session.post("https://community.steam-api.com/ITerritoryControlMinigameService/GetPlayerInfo/v0001/", data=data)
     if result.status_code != 200:
         print("Getting user info errored... trying again(after 10s cooldown)")
         sleep(10)
-        play_game(session)
+        play_game(session, update_check)
     if "active_zone_game" in result.json()["response"]:
         print("Stuck on zone... trying to leave")
-        leave_game(session, result.json()["response"]["active_zone_game"])
+        leave_game(session, result.json()["response"]["active_zone_game"], update_check)
     if "active_planet" in result.json()["response"]:
         return result.json()["response"]["active_planet"]
     else:
         return -1
 
 
-def leave_game(session, current):
+def leave_game(session, current, update_check):
     data = {
         'gameid': current,
         'access_token': get_game_token(session)
@@ -138,10 +136,10 @@ def leave_game(session, current):
     if result.status_code != 200:
         print("Leave planet " + str(current) + " errored... trying again(after 10s cooldown)")
         sleep(10)
-        play_game(session)
+        play_game(session, update_check)
 
 
-def join_planet(session, planet_id, planet_name):
+def join_planet(session, planet_id, planet_name, update_check):
     data = {
         'id': planet_id,
         'access_token': get_game_token(session)
@@ -150,12 +148,12 @@ def join_planet(session, planet_id, planet_name):
     if result.status_code != 200:
         print("Join planet '" + str(planet_name) + "' (" + str(planet_id) + ") errored... trying again(after 10s cooldown)")
         sleep(10)
-        play_game(session)
+        play_game(session, update_check)
     else:
         print("Joined planet: " + str(planet_name) + " (" + str(planet_id) + ")" + "\n")
 
 
-def join_zone(session, zone_position, difficulty):
+def join_zone(session, zone_position, difficulty, update_check):
     dstr = {
         1: 'Easy',
         2: 'Medium',
@@ -170,12 +168,12 @@ def join_zone(session, zone_position, difficulty):
     if result.status_code != 200 or result.json() == {'response':{}}:
         print("Join zone " + str(zone_position) + " errored... trying again(after 1m cooldown)")
         sleep(60)
-        play_game(session)
+        play_game(session, update_check)
     else:
         print("Joined zone: " + str(zone_position) + " (Difficulty: " + dstr.get(difficulty,difficulty) + ")")
 
 
-def report_score(session, difficulty):
+def report_score(session, difficulty, update_check):
     data = {
         'access_token': get_game_token(session),
         'score': 5*120*(2**(difficulty-1)),
@@ -184,32 +182,32 @@ def report_score(session, difficulty):
     result = session.post("https://community.steam-api.com/ITerritoryControlMinigameService/ReportScore/v0001/", data=data)
     if result.status_code != 200 or result.json() == {'response':{}}:
         print("Report score errored... Current zone likely completed...\n")
-        play_game(session)
+        play_game(session, update_check)
     else:
         res = result.json()["response"]
         print("Old Score: " + str(res["old_score"]) + " (Level " + str(res["old_level"]) + ") - " + "New Score: " + str(res["new_score"]) + " (Level " + str(res["new_level"]) + ") - " + "Next Level Score: " + str(res["next_level_score"]) + "\n")
 
 
-def play_game(session):
+def play_game(session, update_check=7):
     print("Checking if user is currently on a planet")
-    current = get_user_info(session)
+    current = get_user_info(session, update_check)
     if current != -1:
         print("Leaving current planet")
-        leave_game(session, current)
+        leave_game(session, current, update_check)
     print("Finding a planet and zone")
     zone_position, planet_id, planet_name, difficulty = get_zone(session)
-    join_planet(session, planet_id, planet_name)
+    join_planet(session, planet_id, planet_name, update_check)
     while 1:
-        join_zone(session, zone_position, difficulty)
+        join_zone(session, zone_position, difficulty, update_check)
         print("Sleeping for 1 minute 50 seconds")
         sleep(110)
-        report_score(session, difficulty)
+        report_score(session, difficulty, update_check)
         update_check = update_check - 1
         if update_check == 0:
             print("Checking for any new planets......")
             update_check = 7
-            play_game(session)
-        get_user_info(session) # get user info and leave game, incase user gets stuck
+            play_game(session, update_check)
+        get_user_info(session, update_check) # get user info and leave game, incase user gets stuck
 
 
 def login(user, secret, previous_code=None):
